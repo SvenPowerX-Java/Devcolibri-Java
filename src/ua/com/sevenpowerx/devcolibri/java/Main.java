73a8423f7da5bf569551a1e4aff2d863ffca6083
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-30 16:21:17 +0100

    Added showHelloDevcolibriJava method the Main class

--- src/ua/com/sevenpowerx/devcolibri/java/Main.java
+++ src/ua/com/sevenpowerx/devcolibri/java/Main.java
@@ -2,6 +2,11 @@ package ua.com.sevenpowerx.devcolibri.java;
 
 public class Main {
 	public static void main(String[] args) {
+//		System.out.println("Hello, Devcolibri Java courses");
+		showHelloDevcolibriJava();
+	}
+	
+	  private static void showHelloDevcolibriJava(){
 		System.out.println("Hello, Devcolibri Java courses");
 	}
 }
