aca57002a4b74cf71047044d6288b41a0246d663
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-30 16:37:41 +0100

    Added new module Variable

---
+++ Variables/src/ua/com/sevenpowerx/java/devcolibri/java/Main.java
@@ -0,0 +1,16 @@
+package ua.com.sevenpowerx.java.devcolibri.java;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Type Variable");
+		
+		byte b;
+		b = 127;
+		System.out.println(b);
+		
+		short sh = 8;
+		System.out.println(sh);
+		
+		
+	}
+}
